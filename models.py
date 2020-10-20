c5348bf576b7791fafcf3233ac8123934537e3d7
barnez299 <barnez29@gmail.com>
2020-10-20 23:19:58 +0200

    updated models to include workout table linked to user

--- models.py
+++ models.py
@@ -1,5 +1,7 @@
-from . import db
 from flask_login import UserMixin
+from datetime import datetime
+from . import db
+
 
 
 class User(db.Model, UserMixin):
@@ -15,6 +17,23 @@ class User(db.Model, UserMixin):
 
     def __repr__(self):
         return f'{self.name} : {self.email}'
+
+
+class Workout(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    pushups = db.Column(db.Integer, nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    comment = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __init__(self, pushups, date_posted, comment, user_id):
+        self.pushups = pushups
+        self.date_posted = date_posted
+        self.comment = comment
+        self.user_id = user_id
+
+    def __repr__(self):
+        return f'{self.user_id}'
 
 
 
