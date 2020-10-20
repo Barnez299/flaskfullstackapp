e52e4730202c90dc9397bdc925dccdc076353a6a
barnez299 <barnez29@gmail.com>
2020-10-20 11:06:28 +0200

    database created successfully

--- __init__.py
+++ __init__.py
@@ -5,17 +5,24 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 
+
+
 def create_app():
     app = Flask(__name__)
 
+    # set up db instance
+    app.config['SECRET_KEY'] = 'secret-key-goes-here'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123456789@localhost:5432/user'
+
+    db = SQLAlchemy(app)
+    db.init_app(app)
+
 
     # register main blueprint
-
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
     # register auth blueprint
-
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint)
 
---
+++ models.py
@@ -0,0 +1,15 @@
+from . import db
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)  # primary keys are required by SQLAlchemy
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+
+    def __repr__(self):
+        return f'{self.name} : {self.email}'
